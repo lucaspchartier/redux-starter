04a9c8e3b88b64c33d6d6d35dc818519b8c1b714
lucaspchartier <lucaspchartier@gmail.com>
2021-01-26 19:36:49 -0500

    chore(src/app.js): add string methods to demonstrate functional programming

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+let input = "  JavaScript  ";
+let output = "div" + input.trim() + "/div";
+
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`;
+const toLowerCase = str => str.toLowerCase();
+
+const result = wrapInDiv(toLowercase(trim(input)));
