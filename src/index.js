57f865ca9baf128f29ba54d4987cbd6e1c634156
lucaspchartier <lucaspchartier@gmail.com>
2021-01-28 18:41:46 -0500

    chore(index.js): create new branch objects and begin adding person object

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import { compose, pipe } from 'lodash/fp';
-
-let input = "  JavaScript  ";
-let output = "div" + input.trim() + "/div";
-
-const trim = str => str.trim();
-const wrap = type => str => `<${type}>${str}</${type}>`;
-const toLowerCase = str => str.toLowerCase();
-
-const transform = pipe(trim, toLowerCase, wrap("span"));
-console.log(transform(input));
+const person = { name: "John" };
+const updated = Object.assign({}, person, { name: "Bob", age: 30 } );
+console.log(updated);
