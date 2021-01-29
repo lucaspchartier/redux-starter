13587bc984bb4fce24b30abaa5eccb508b749edc
lucaspchartier <lucaspchartier@gmail.com>
2021-01-29 07:02:14 -0500

    chore(src/index.js): create immutable branch and update index.js with book object and publish function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-const numbers = [1, 2, 3];
+let book = { title: "Harry Potter" };
 
-// Adding
-const index = numbers.indexOf(2);
-const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
-console.log(added);
+const publish = book => {
+    book.isPublished = true;
+};
 
-// Removing
-const removed = numbers.filter(n => n !== 2);
-console.log(removed);
+publish(book);
 
-// Updating
-const updated = numbers.map(n => n === 2 ? 20 : n);
-console.log(updated);
+console.log(book);
