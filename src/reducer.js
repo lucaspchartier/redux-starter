8b6ef11d0ab53363bfe82e053038bc3fab5b04a0
lucaspchartier <lucaspchartier@gmail.com>
2021-02-01 20:02:19 -0500

    chore(src/reducer.js): replace if/else statement with switch function in reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,18 +1,17 @@
 let lastId = 0;
 
-const reducer = (state, action) => {
-    if (action.type === "bugAdded") {
-        return [
-            ...state,
-            {
-                id: ++lastId,
-                description: action.payload.description,
-                resolved: false
-            }
-        ];
-    }
-
-    else if (action.type === "bugRemoved") {
-        return state.filter(bug => bug.id !== action.payload.id);
+const reducer = (state = [], action) => {
+    switch (action.type) {
+        case "bugAdded":
+            [
+                ...state,
+                {
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ];
+        case "bugRemoved":
+            state.filter(bug => bug.id !== action.payload.id);
     }
 };
