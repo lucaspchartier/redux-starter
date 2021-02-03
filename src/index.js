935d964144d786646a626ab79d0ac4abba42d2e2
lucaspchartier <lucaspchartier@gmail.com>
2021-02-02 19:44:39 -0500

    chore(src/index.js): import store object from source.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import { compose, pipe } from 'lodash/fp';
-
-let input = "  JavaScript  ";
-let output = "div" + input.trim() + "/div";
-
-const trim = str => str.trim();
-const wrap = type => str => `<${type}>${str}</${type}>`;
-const toLowerCase = str => str.toLowerCase();
-
-const transform = pipe(trim, toLowerCase, wrap("span"));
-console.log(transform(input));
+import store from "./source";
