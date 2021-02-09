64aaa32d6a6d945dda021eb37eb505da028607e8
lucaspchartier <lucaspchartier@gmail.com>
2021-02-08 19:51:09 -0500

    chore(src/index.js): add dispatch method for removing bug in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import store from "./source";
 
 store.dispatch({
-    
+    type: "bugAdded",
+    payload: {
+        description: "Bug1"
+    }
+});
+
+store.dispatch({
+    type: "bugRemoved",
+    id: 1
 });
 
 console.log(store.getState());
