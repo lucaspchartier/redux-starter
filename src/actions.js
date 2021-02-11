a56eb627e9579e357415a4e0bf31279c49d63bc0
lucaspchartier <lucaspchartier@gmail.com>
2021-02-10 20:08:49 -0500

    chore(src/actions.js): create bugRemoved function in actions component to export into index component

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,13 @@
 import * as actions from "./actionTypes";
 
-export const bugAdded = description => {
-    return {
-        type: actions.BUG_ADDED,
-        payload: {
-            description: "Bug1"
-        }
-    };
-};
+export const bugAdded = description => ({
+    type: actions.BUG_ADDED,
+    payload: {
+        description: "Bug1"
+    }
+});
+
+export const bugRemoved = description => ({
+    type: actions.BUG_REMOVED,
+    id: 1
+});
