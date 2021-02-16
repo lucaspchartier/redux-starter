cb9e240f2cef87aafbd5b1033238065fc23c8ac3
lucaspchartier <lucaspchartier@gmail.com>
2021-02-16 18:52:04 -0500

    chore(src/actions.js): use only single key-value pair in objects using argument being passed into functions

--- src/actions.js
+++ src/actions.js
@@ -3,14 +3,14 @@ import * as actions from "./actionTypes";
 export const bugAdded = description => ({
     type: actions.BUG_ADDED,
     payload: {
-        description: "Bug1"
+        description
     }
 });
 
 export const bugResolved = id => ({
     type: actions.BUG_RESOLVED,
     payload: {
-        id: id,
+        id,
         resolved: true
     }
 });
